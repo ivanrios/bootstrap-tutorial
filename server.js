e260135b0bfcb6e81f773ca02b3bb8d3a6bba65c
Ivan Rios <ivanrios.a@gmail.com>
2016-03-11 15:45:15 -0500

    Quitando cambios se me fue un archivoi Revert "Agregar un servidor web"
    This reverts commit 976e4e3139e9cd83a5d8746e583eae59126935de.

--- server.js
+++
@@ -1,4 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3000);
-console.log("Listo, entra a http://localhost:3000/www/landing.html");
